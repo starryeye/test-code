69dc0d7b69b15ee78ba84e39c31e9dace07e1ff6
starryeye <see2818@naver.com>
2023-07-20 00:26:59 +0900

    [starryeye, 2023.07.20] - 주석 추가
    내용
    - 주석 추가

--- cafekiosk/src/main/java/dev/practice/cafekiosk/unit/CafeKiosk.java
+++ cafekiosk/src/main/java/dev/practice/cafekiosk/unit/CafeKiosk.java
@@ -44,7 +44,7 @@ public class CafeKiosk {
 
         return beverages.stream()
                 .mapToInt(Beverage::getPrice)
-                .sum(); 
+                .sum();
     }
 
     public Order createOrder() {
@@ -58,6 +58,17 @@ public class CafeKiosk {
         return new Order(LocalDateTime.now(), beverages);
     }
 
+    /**
+     * 테스트하기 어려운 영역을 구분하고 분리할 줄 알아야한다.
+     * - 테스트 하기 쉬운 코드에 테스트가 어려운 코드가 생기면 테스트 쉬웠던 전체가 테스트하기 어려워진다.
+     * - 테스트 어려운 코드를 외부로 분리해보자.. (createOrder 메서드의 현재 시간을 파라미터로 받는 조치를 취함)
+     * - 가만히 생각해보면 우리가 테스트하려고 하는 영역은 현재 시간이 아니라 어떤 시간이 주어졌을때 우리가 개발한 로직으로 주문 생성을 수행하느냐 이다.
+     * - 테스트 어려운 코드를 계속 외부로 분리하면 테스트 가능한 코드는 많아진다.. (현재시간을 받는 부분을 계속 콜 스택 타고 올린다..)
+     * - 어디까지 외부 세계로 넘겨야하나...? 더이상 넘길수 없을 때까지...
+     *
+     * 테스트 하기 어려운 영역 (Adapter)
+     * 테스트하기 쉬운 영역 (Application)
+     */
     public Order createOrder(LocalDateTime currentDate) {
 
         LocalTime currentTime = currentDate.toLocalTime();
