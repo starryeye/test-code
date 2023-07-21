57951458e6da95b01a3a0b8f1338c7d0b7c841cc
starryeye <see2818@naver.com>
2023-07-21 23:27:29 +0900

    [starryeye, 2023.07.21] - Domain 개발
    내용
    - layered architecture

---
+++ cafekiosk/src/main/java/dev/practice/cafekiosk/spring/domain/product/Product.java
@@ -0,0 +1,28 @@
+package dev.practice.cafekiosk.spring.domain.product;
+
+import dev.practice.cafekiosk.spring.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends BaseEntity  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    @Enumerated(value = EnumType.STRING) // HANDMADE, BOTTLE, BAKERY
+    private ProductType type;
+
+    @Enumerated(value = EnumType.STRING) // SELLING, HOLD, STOP_SELLING
+    private ProductSellingType sellingType;
+
+    private String name;
+    private int price;
+}
