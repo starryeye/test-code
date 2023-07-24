8e7ae257afd3d6f5b260830cb65377f4014daae8
starryeye <see2818@naver.com>
2023-07-24 22:08:26 +0900

    [starryeye, 2023.07.24] - TDD, Product Service 개발
    내용
    - Red 단계

--- cafekiosk/src/test/java/dev/practice/cafekiosk/spring/api/service/product/ProductServiceTest.java
+++ cafekiosk/src/test/java/dev/practice/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -54,6 +54,27 @@ class ProductServiceTest {
                 .contains("002", HANDMADE, SELLING, "카푸치노", 5000);
     }
 
+
+    @DisplayName("상품이 하나도 없는 경우 신규 상품을 등록하면 상품번호는 001 이다.")
+    @Test
+    void createProductWhenProductsIsEmpty() {
+
+        // given
+        ProductCreateRequest productCreateRequest = ProductCreateRequest.builder()
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("카푸치노")
+                .price(5000)
+                .build();
+
+        // when
+        ProductResponse savedProduct = productService.createProduct(productCreateRequest);
+
+        // then
+        assertThat(savedProduct).extracting("productNumber", "type", "sellingStatus", "name", "price")
+                .contains("001", HANDMADE, SELLING, "카푸치노", 5000);
+    }
+
     private Product createProduct(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
 
         return Product.builder()
