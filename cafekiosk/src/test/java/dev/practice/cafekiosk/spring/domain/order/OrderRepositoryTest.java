d9458049c493952954e4c6b3f3fd96a40408e7ac
starryeye <see2818@naver.com>
2023-07-26 02:56:32 +0900

    [starryeye, 2023.07.26] - OrderRepository Test 작성
    내용
    - 주어진 날짜 범위와 주문 상태 조건으로 주문 엔티티를 조회한다.
    - 데이터가 존재하지 않으면 빈 리스트를 반환한다.

--- cafekiosk/src/test/java/dev/practice/cafekiosk/spring/domain/order/OrderRepositoryTest.java
+++ cafekiosk/src/test/java/dev/practice/cafekiosk/spring/domain/order/OrderRepositoryTest.java
@@ -4,27 +4,76 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static dev.practice.cafekiosk.spring.domain.order.OrderStatus.INIT;
+import static dev.practice.cafekiosk.spring.domain.order.OrderStatus.PAYMENT_COMPLETED;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @DataJpaTest
 class OrderRepositoryTest {
 
     @Autowired
     private OrderRepository orderRepository;
 
-    //해피 케이스
-    //해피 케이스 빈 리스트
-    //예외 케이스 X/O/X
-    //예외 케이스 /OX/
-
-    @DisplayName("")
+    @DisplayName("주어진 날짜 범위와 주문 상태 조건으로 주문 엔티티를 조회한다.")
     @Test
-    void test() {
+    void findOrdersBy() {
 
         // given
+        LocalDateTime now = LocalDateTime.of(2023, 7, 26, 0, 0, 0);
+
+        Order order1 = createOrderWithEmptyProducts(LocalDateTime.of(2023, 7, 25, 23, 59, 59), PAYMENT_COMPLETED);
+        Order order2 = createOrderWithEmptyProducts(now, PAYMENT_COMPLETED);
+        Order order3 = createOrderWithEmptyProducts(now, INIT);
+        Order order4 = createOrderWithEmptyProducts(LocalDateTime.of(2023, 7, 26, 23, 59, 59), PAYMENT_COMPLETED);
+        Order order5 = createOrderWithEmptyProducts(LocalDateTime.of(2023, 7, 27, 0, 0, 0), PAYMENT_COMPLETED);
+        orderRepository.saveAll(List.of(order1, order2, order3, order4, order5));
+
         // when
+        List<Order> orders = orderRepository.findOrdersBy(
+                LocalDateTime.of(2023, 7, 26, 0, 0, 0),
+                LocalDateTime.of(2023, 7, 27, 0, 0, 0),
+                PAYMENT_COMPLETED
+        );
+
         // then
+        assertThat(orders).hasSize(2)
+                .extracting("registeredAt", "orderStatus")
+                .containsExactlyInAnyOrder(
+                        tuple(LocalDateTime.of(2023, 7, 26, 0, 0, 0), PAYMENT_COMPLETED),
+                        tuple(LocalDateTime.of(2023, 7, 26, 23, 59, 59), PAYMENT_COMPLETED)
+                );
+
+    }
+
+    @DisplayName("데이터가 존재하지 않으면 빈 리스트를 반환한다.")
+    @Test
+    void findEmptyOrdersBy() {
+
+        // when
+        List<Order> orders = orderRepository.findOrdersBy(
+                LocalDateTime.of(2023, 7, 26, 0, 0, 0),
+                LocalDateTime.of(2023, 7, 27, 0, 0, 0),
+                PAYMENT_COMPLETED
+        );
+
+        // then
+        assertThat(orders).hasSize(0);
+    }
+
+    private Order createOrderWithEmptyProducts(LocalDateTime now, OrderStatus orderStatus) {
+        return Order.builder()
+                .products(List.of())
+                .orderStatus(orderStatus)
+                .registeredAt(now)
+                .build();
     }
 
 }
