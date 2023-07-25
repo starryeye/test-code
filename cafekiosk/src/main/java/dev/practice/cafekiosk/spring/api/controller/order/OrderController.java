ef9e8795ea36c7751c14f48f4e2e30b4beab40ae
starryeye <see2818@naver.com>
2023-07-25 21:35:27 +0900

    [starryeye, 2023.07.25] - Controller Binding Test
    내용
    - Controller Binding Test

--- cafekiosk/src/main/java/dev/practice/cafekiosk/spring/api/controller/order/OrderController.java
+++ cafekiosk/src/main/java/dev/practice/cafekiosk/spring/api/controller/order/OrderController.java
@@ -1,8 +1,10 @@
 package dev.practice.cafekiosk.spring.api.controller.order;
 
+import dev.practice.cafekiosk.spring.api.ApiResponse;
 import dev.practice.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import dev.practice.cafekiosk.spring.api.service.order.OrderService;
 import dev.practice.cafekiosk.spring.api.service.order.response.OrderResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,8 +19,8 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public OrderResponse createOrder(@RequestBody OrderCreateRequest orderCreateRequest) {
+    public ApiResponse<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest orderCreateRequest) {
         LocalDateTime registeredAt = LocalDateTime.now();
-        return orderService.createOrder(orderCreateRequest, registeredAt);
+        return ApiResponse.ok(orderService.createOrder(orderCreateRequest, registeredAt));
     }
 }
