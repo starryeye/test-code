daff5b4cbcc95942c5a368ed96dc0983b71abe4e
starryeye <see2818@naver.com>
2023-07-28 23:28:27 +0900

    [starryeye, 2023.07.28] - DynamicTest
    내용
    - @TestFactory

--- cafekiosk/src/test/java/dev/practice/cafekiosk/spring/domain/stock/StockTest.java
+++ cafekiosk/src/test/java/dev/practice/cafekiosk/spring/domain/stock/StockTest.java
@@ -1,7 +1,12 @@
 package dev.practice.cafekiosk.spring.domain.stock;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Collection;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -53,6 +58,59 @@ class StockTest {
                 () -> stock.deductQuantity(quantity)
         ).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("차감할 재고 수량이 없습니다.");
+    }
+
+
+    /**
+     * DynamicTest 는 시나리오 테스트를 수행할때 사용하면 좋다.
+     * 아래는 기본 구조이다.
+     */
+    @DisplayName("")
+    @TestFactory
+    Collection<DynamicTest> dynamicTest() {
+
+        return List.of(
+                DynamicTest.dynamicTest("", () -> {
+
+                }),
+                DynamicTest.dynamicTest("", () -> {
+
+                })
+        );
+    }
+
+    /**
+     * DynamicTest 로 시나리오 테스트를 수행할 수 있다.
+     * Stock 변수를 공용자원으로 사용하여 재고 수량을 바꿔가면서 테스트를 하고 있다.
+     */
+    @DisplayName("재고 차감 시나리오")
+    @TestFactory
+    Collection<DynamicTest> stockDeductionDynamicTest() {
+
+        // given
+        Stock stock = Stock.create("001", 1);
+
+        return List.of(
+                DynamicTest.dynamicTest("재고를 주어진 개수만큼 차감할 수 있다.", () -> {
+                    // given
+                    int quantity = 1;
+
+                    // when
+                    stock.deductQuantity(quantity);
+
+                    // then
+                    assertThat(stock.getQuantity()).isZero();
+                }),
+                DynamicTest.dynamicTest("재고보다 많은 수의 수량으로 차감 시도하는 경우에는 예외가 발생한다.", () -> {
+                    // given
+                    int quantity = 1;
+
+                    // when // then
+                    assertThatThrownBy(() -> stock.deductQuantity(quantity))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessage("차감할 재고 수량이 없습니다.");
+                })
+        );
     }
 
 }
