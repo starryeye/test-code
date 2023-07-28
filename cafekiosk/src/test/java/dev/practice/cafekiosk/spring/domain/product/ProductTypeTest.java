c20f2c9a7139e31436b02390235fe8baefa4df08
starryeye <see2818@naver.com>
2023-07-28 23:12:17 +0900

    [starryeye, 2023.07.28] - @ParameterizedTest
    내용
    - @CsvSource, @MethodSource

--- cafekiosk/src/test/java/dev/practice/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ cafekiosk/src/test/java/dev/practice/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -2,6 +2,12 @@ package dev.practice.cafekiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,6 +33,49 @@ class ProductTypeTest {
         assertThat(result1).isFalse();
         assertThat(result2).isTrue();
         assertThat(result3).isTrue();
+    }
+
+    /**
+     * csv 는 콤마를 기준으로 split 하여 구분하는 것
+     * 따라서, HANDMADE,false 는 아래 테스트 메서드의 각각 파라미터로 넣어진다.
+     * @ParameterizedTest
+     * 하나의 테스트 케이스에서 여러 값으로 돌아가며 테스트하고 싶을때 사용한다.
+     */
+    @DisplayName("상품 타입이 재고 관련 타입인지 확인한다.")
+    @CsvSource({"HANDMADE,false", "BOTTLE,true", "BAKERY,true"})
+    @ParameterizedTest
+    void containStockType2(ProductType productType, boolean expected) {
+
+        //when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+
+    }
+
+    private static Stream<Arguments> provideProductTypesForCheckingStockType() {
+        return Stream.of(
+                Arguments.of(ProductType.HANDMADE, false),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.BAKERY, true)
+        );
+    }
+
+    /**
+     * @MethodSource 로 @ParameterizedTest 수행
+     */
+    @DisplayName("상품 타입이 재고 관련 타입인지 확인한다.")
+    @MethodSource(value = "provideProductTypesForCheckingStockType")
+    @ParameterizedTest
+    void containStockType3(ProductType productType, boolean expected) {
+
+        //when
+        boolean result = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+
     }
 
 }
