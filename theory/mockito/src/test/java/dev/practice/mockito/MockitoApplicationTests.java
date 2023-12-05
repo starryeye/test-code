d0d9556f4e57a059073e367b65c2de479d1b6417
starryeye <see2818@naver.com>
2023-12-05 23:08:18 +0900

    [starryeye, 2023.12.05] - mockito library study init
    내용
    - mockito library study init

---
+++ theory/mockito/src/test/java/dev/practice/mockito/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
